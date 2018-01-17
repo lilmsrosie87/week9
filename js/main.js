ad5342bb9b1477d2159e2aea9d19518c3a946ac2
Roslyn Harris <rharris0113@gmail.com>
2018-01-16 20:56:37 -0800

    working on week 9 excercises. Stopped at #6

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// was overriding before correction .class in html, but still not working. Trying to add an output so hopefully it will show
+function colorChange() {
+	document.getElementById("cotDamn").backgroundColor = "#cc99ff";
+}
+
+document.write(colorChange())
+
+// stopped at #6
